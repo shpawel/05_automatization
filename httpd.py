c9f94385162133abb6f5c19b68b30e64593ba394
Шиниев Павел <shpawel@mail.ru>
2018-05-13 12:57:59 +0300

    учёт замечаний

--- httpd.py
+++ httpd.py
@@ -15,11 +15,10 @@ logger = logging.getLogger(__name__)
 
 
 LOG_FORMAT = '%(levelname) -10s %(asctime)s %(name) -30s %(funcName) -35s %(lineno) -5d: %(message)s'
-HOST = 'localhost'
-PORT = 80
 IP = 'localhost'
+PORT = 80
 ADDR = (IP, PORT)
-DOCUMENT_ROOT = os.path.abspath(os.path.join(os.path.curdir, 'test'))
+DOCUMENT_ROOT = os.path.abspath(os.path.join(os.path.curdir, 'tests'))
 
 RE_HEAD = re.compile(r'^(?P<method>\w*) (?P<address>.*) (?P<protocol>HTTP\/\d+\.\d+)$')
 
@@ -40,8 +39,8 @@ class HTTPD(object):
         :key root: корневая директория
         """
 
-        self._server_address = (kwargs.pop('host', None) or HOST, kwargs.pop('port', None) or PORT)
-        self._timeout = kwargs.pop('timeout', None) or 100.0
+        self._server_address = (kwargs.pop('host', None) or IP, kwargs.pop('port', None) or PORT)
+        self._timeout = kwargs.pop('timeout', None) or float(10.0)
         self._root = kwargs.pop('root', DOCUMENT_ROOT)
         self._sock = None
         self.connect()
@@ -61,6 +60,7 @@ class HTTPD(object):
             self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
             self._sock.setblocking(0)
             self.bind()
+            self.listen()
         except socket.error as e:
             raise HTTPDException(e)
 
@@ -87,7 +87,6 @@ class HTTPD(object):
         """
         self._sock.bind(ADDR)
         logging.info('Bind %s', str(ADDR))
-        self.listen()
 
     def listen(self):
         """
@@ -95,7 +94,7 @@ class HTTPD(object):
 
         :return:
         """
-        self._sock.listen(True)
+        self._sock.listen(1)
         self._sock.settimeout(self._timeout)
         logging.info('Listen %s ', str(self._sock.getsockname()))
 
@@ -110,8 +109,6 @@ class HTTPD(object):
         messages = {}
         while inputs:
             readable, writable, exceptional = select.select(inputs, outputs, inputs, self._timeout)
-            if not (readable or writable or exceptional):
-                continue
 
             for r in readable:
                 if r is self._sock:
@@ -177,7 +174,6 @@ class HTTPD(object):
         request_data = b''
 
         while end_nrn not in request_data:
-            request_data = None
             try:
                 temp = connection.recv(1024)
             except Exception as e:
@@ -211,48 +207,55 @@ class HTTPD(object):
             return b'Requies is not valid'
 
         if method not in ['HEAD', 'GET']:
-            message = self.parse_response_error(status=b'405 Method Not Allowed',
-                                                data=b'Method Not Allowed',
-                                                type_data=b'text/plain')
+            message = self.parse_response(method=method,
+                                          status=b'405 Method Not Allowed',
+                                          data=b'Method Not Allowed',
+                                          type_data=b'text/plain')
         else:
             p = urlparse(address)
             u = unquote(p.path)
             logging.info('URL %s', str(u))
-            message = self.parse_response(method=method, path=u)
+            status, data, type_data = self.get_data(path=u)
+            message = self.parse_response(method=method,
+                                          status=status,
+                                          data=data,
+                                          type_data=type_data)
         return message
 
-    def parse_response_error(self, status, data, type_data=b'text/plain'):
+    def parse_response(self, method, status, data, type_data=b'text/plain'):
         """
-        Обратное сообщение об ошибке
+        Формирование ответного сообщения
 
-        :param socket._socketobject conn: объект подключения
-        :param bytes status: статут
-        :param bytes data: тело сообщения
-        :param bytes type_data: тип тела сообщения
-        :return: обратное сообщение для клиента
+        :param str|unicode method: тип HTTP-запроса
+        :param str|unicode status: статус ответа
+        :param str|unicode data: передаваемые данные
+        :param str|unicode type_data: тип передаваемых данных
+        :return: ответное сообщение
         :rtype: bytes
         """
-        data = data.encode('utf-8')
         response = b''
         response += b'HTTP/1.1 ' + status.encode('utf-8') + b'\r\n'
-        response += b'Date: ' + time.strftime('%H:%M:%S %d.%m.%Y') + '\r\n'
+        response += b'Accept: */*' + '\r\n'
+        response += b'Accept-Encoding: *' + '\r\n'
+        response += b'Connection: keep alive' + b'\r\n'
         response += b'Server: HTTPD 1/.0' + '\r\n'
-        response += b'Connection: keep alive' + '\r\n'
-        response += b'Content-Type: ' + type_data.encode('utf-8') + b'\r\n'
-        response += b'Content-Length: ' + bytes(len(data)) + b'\r\n'
         response += b'Allow: GET, HEAD' + b'\r\n'
+        response += b'Content-Length: ' + bytes(len(data)) + b'\r\n'
+        response += b'Content-Type: ' + type_data.encode('utf-8') + b'\r\n'
+        response += b'Date: ' + time.strftime('%H:%M:%S %d.%m.%Y') + '\r\n'
         response += b'\r\n'
-        response += data
+        if method != 'HEAD':
+            response += data
         return response
 
-    def parse_response(self, method, path):
+    def get_data(self, path):
         """
-        Обратное сообщение
+        Получение запрашиваемых данных
 
         :param str|unicode method: метод запроса сообщения
         :param str|unicode path: путь к запрашиваемому файлу или директории
-        :return: обратное сообщение для клиента
-        :rtype: bytes
+        :return: статус, данные, тип передаваемых данных
+        :rtype: tuple([bytes], [bytes], [bytes])
         """
         _path = os.path.abspath(path).strip('/')
         source = os.path.abspath(os.path.join(self._root, _path))
@@ -262,25 +265,12 @@ class HTTPD(object):
         if os.path.exists(source):
             data = open(source, mode='rb').read()
             content_type = mimetypes.guess_type(source)[0]
-            content_size = bytes(len(data))
             status = "200 OK"
         else:
             data = b'Not Found'
             content_type = 'text/plain'
-            content_size = bytes(len(data))
-            status = b'404 Not Found'
-
-        response = b''
-        response += b'HTTP/1.1 ' + status.encode('utf-8') + b'\r\n'
-        response += b'Server: HTTPD' + b'\r\n'
-        response += b'Date: ' + time.strftime("%H:%M:%S %d.%m.%Y") + b'\r\n'
-        response += b'Connection: keep alive' + b'\r\n'
-        response += b'Content-Type: ' + content_type.encode('utf-8') + b'\r\n'
-        response += b'Content-Length: ' + content_size.encode('utf-8') + b'\r\n'
-        response += b'\r\n'
-        if method != 'HEAD':
-            response += data
-        return response
+            status = '404 Not Found'
+        return status, data, content_type
 
     def __call__(self, *args, **kwargs):
         self.run()
@@ -290,18 +280,30 @@ def main():
     import argparse  # noqa
 
     parser = argparse.ArgumentParser()
+    parser.add_argument('--host',
+                        help='Адрес сервера',
+                        default=IP,
+                        type=str)
+    parser.add_argument('--port',
+                        help='Порт сервера',
+                        default=PORT,
+                        type=int)
     parser.add_argument('-w', '--workers',
                         help='Число создаваемых потоков',
-                        default=4)
+                        default=4,
+                        type=int)
     parser.add_argument('-t', '--timeout',
                         help='Таймаут ожидания ответа',
-                        default=60)
+                        default=60.0,
+                        type=float)
     parser.add_argument('-r', '--root',
                         help='Директория по умолчанию',
                         default=DOCUMENT_ROOT)
 
     args = parser.parse_args()
     params = dict(
+        host=args.host,
+        port=args.port,
         root=args.root,
         workers=args.workers,
         timeout=args.timeout
@@ -309,12 +311,14 @@ def main():
 
     pool = []
     for i in range(args.workers):
-        _th = threading.Thread(target=HTTPD(**params), daemon=True)
+        _th = threading.Thread(target=HTTPD(**params))
         pool.append(_th)
 
     try:
         for _th in pool:
             _th.start()
+
+        for _th in pool:
             _th.join()
     except Exception as e:
         logger.exception(str(e))
@@ -323,5 +327,5 @@ def main():
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+    logging.basicConfig(level=logging.ERROR, format=LOG_FORMAT)
     main()
